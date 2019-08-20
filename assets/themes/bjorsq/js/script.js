42ce4a140efa5fb93158db478a3339a9bc2e1198
bjorsq <pete@bjorsq.net>
2019-08-20 23:28:39 +0100

    Made vimeo API call over HTTPS

--- assets/themes/bjorsq/js/script.js
+++ assets/themes/bjorsq/js/script.js
@@ -188,7 +188,7 @@
 
     /* vimeo wall */
 	if ($('#vimeo_wall').length) {
-		$.getJSON('http://vimeo.com/api/v2/bjorsq/videos.json', function(data){
+		$.getJSON('https://vimeo.com/api/v2/bjorsq/videos.json', function(data){
 			var html = '';
 			$.each( data, function() {
 				html += '<a href="'+this.url+'" class="video-link" target="_blank"><img src="'+this.thumbnail_large+'" /><span>'+this.title+'</span></a>';
