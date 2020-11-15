0c059973b778ccb41f0e373eeec69cf31ff21c12
bjorsq <pete@bjorsq.net>
2020-11-15 20:51:51 +0000

    Added date sorting

--- assets/themes/bjorsq/js/script.js
+++ assets/themes/bjorsq/js/script.js
@@ -52,7 +52,18 @@
 		$('ul.bq-list li').each(function(){
 			types[$(this).data('tags')].push($(this));
 		});
-		$('.bq-list').before('<p>Sort <a href="#" class="sort-az">A-Z</a> or group by <a href="#" class="sort-type">type</a></p>');
+		$('.bq-list').before('<p>Sort by <a href="#" class="sort-link sort-date sort-active">date</a>, <a href="#" class="sort-link sort-az">a-z</a> or <a href="#" class="sort-link sort-type">group by type</a></p>');
+		$(document).on('click', '.sort-date', function(e){
+			e.preventDefault();
+			if ($('.bq-list').length>1) {
+				ungroup();
+			}
+			$('.bq-list').removeClass('sorted-desc').removeClass('sorted-asc');
+			sortList($('.bq-list'),true,'date');
+			$('.sort-link').removeClass('sort-active');
+			$('.sort-az').text('a-z');
+			$(this).addClass('sort-active');
+		});
 		$(document).on('click', '.sort-az', function(e){
 			e.preventDefault();
 			if ($('.bq-list').length>1) {
@@ -60,8 +71,11 @@
 			}
 			var sortDescending = $(this).parent().next('.bq-list').hasClass('sorted-asc');
 			var newClass = sortDescending? 'sorted-desc': 'sorted-asc';
+			var newText =  sortDescending? 'a-z': 'z-a';
 			$('.bq-list').removeClass('sorted-desc').removeClass('sorted-asc').addClass(newClass);
 			sortList($('.bq-list'),sortDescending);
+			$('.sort-link').removeClass('sort-active');
+			$(this).addClass('sort-active').text(newText);
 		});
 		$(document).on('click', '.sort-type', function(e){
 			e.preventDefault();
@@ -77,6 +91,9 @@
 			$('.bq-list').each(function(){
 				sortList($(this));
 			});
+			$('.sort-link').removeClass('sort-active');
+			$('.sort-az').text('a-z');
+			$(this).addClass('sort-active');
 		});
 	}
 	function ungroup(){
@@ -85,11 +102,16 @@
 		$('.bq-list,.bq-list-header').remove();
 		$('main').append(newList);
 	}
-	function sortList(list, sortDescending) {
+	function sortList(list, sortDescending, by) {
 		var sortdir = sortDescending? -1: 1;
+		var sortby = (by && by === 'date')? 'date': 'title';
 		var listitems = list.children('li').get();
 		listitems.sort(function(a, b) {
-   			return sortdir * $(a).data('title').toUpperCase().localeCompare($(b).data('title').toUpperCase());
+			if ('title' === sortby){
+				return sortdir * $(a).data(sortby).toUpperCase().localeCompare($(b).data(sortby).toUpperCase());
+			} else {
+				return $(b).data(sortby) - $(a).data(sortby);
+			}
 		});
 		$.each(listitems, function(idx, itm) { list.append(itm); });
 	}
