1e9ce5c916240750f1dae949383457c030ec2384
bjorsq <pete@bjorsq.net>
2022-10-25 08:20:15 +0100

    Removed jquery

--- _posts/2022-10-12-hot-water-pastry.md
+++ _posts/2022-10-12-hot-water-pastry.md
@@ -23,6 +23,6 @@ method
 2. Add half the butter (or the vegan margarine) to the flour and rub in to form crumbs,
 3. Add the other half of the butter (or vegetable shortening) to the water in a pan a bring to a rolling boil.
 4. Add the water / fat mixture to the flour to form a dough.
-5. Split the dough to make the pie base (&frac23;) and lid (&frac13;) and roll out on a floured surface.
+5. Split the dough to make the pie base (2/3) and lid (1/3) and roll out on a floured surface.
 6. Line a pie dish, add the filling, and pinch the lid onto the base after moistening the two joining surfaces.
 7. Bake in a preheated oven at 190&#8451; (375&#8451;) for 40 minutes, then take out of the case, glaze with egg, and place back in the oven for a further 10 minutes.
--- assets/themes/bjorsq/js/script.js
+++ assets/themes/bjorsq/js/script.js
@@ -1,92 +1,134 @@
-/**
- * Flickr interface using jQuery
- * gets a set of albums from Flickr, and provides a means to browse the photos in them
- * using the Flickr API
- */
-(function($){
+window.addEventListener('DOMContentLoaded', event => {
     /* vimeo wall */
-	if ($('#vimeo_wall').length) {
-		$.getJSON('https://vimeo.com/api/v2/bjorsq/videos.json', function(data){
-			var html = '';
-			$.each( data, function() {
-				html += '<a href="'+this.url+'" class="video-link" target="_blank"><img src="'+this.thumbnail_large+'" /><span>'+this.title+'</span></a>';
-			});
-			$('#vimeo_wall').append(html);
+	let vimeo_wall = document.getElementById('vimeo_wall');
+	if ( vimeo_wall !== null ) {
+		let request = new XMLHttpRequest();
+		request.open( 'GET', 'https://vimeo.com/api/v2/bjorsq/videos.json', true );
+		request.addEventListener( 'load', function() {
+			console.log(request);
+  			if ( request.status >= 200 && request.status < 400 ) {
+				console.log(request.responseText);
+			    try {
+					let data = JSON.parse( request.responseText );
+					if ( data.length ) {
+						let html = '';
+						data.forEach( vid => {
+							html += '<a href="'+vid.url+'" class="video-link" target="_blank"><img src="'+vid.thumbnail_large+'" /><span>'+vid.title+'</span></a>';
+						});
+						vimeo_wall.innerHTML = html;
+					}
+				} catch(e) {}
+			}
 		});
+		request.send();
 	}
 	/* podcast */
-	if ($('#podcast').length) {
-		var audioElement = document.createElement('audio');
-		$('#podcast>li a').before('<button class="playbutton"></button>');
-		$(document).on('click','.playbutton', function(){
-			$('.playbutton').not(this).removeClass('playing');
-			audioElement.pause();
-			if ( ! $(this).hasClass('playing') ) {
-				var audioURL = $(this).parents('li').find('a').attr('href');
-				if ( audioElement.getAttribute('src') != audioURL ) {
-					audioElement.setAttribute('src', audioURL);
+	const podcast_list = document.getElementById('podcast');
+	if ( podcast_list !== null ) {
+		const audioElement = document.createElement( 'audio' );
+		podcast_list.querySelectorAll( 'li a' ).forEach( mp3link => {
+			let playbutton = document.createElement( 'button' );
+			playbutton.classList.add( 'playbutton' );
+			playbutton.setAttribute( 'data-audiourl', mp3link.getAttribute( 'href' ) );
+			mp3link.parentNode.insertBefore( playbutton, mp3link );
+		});
+		document.addEventListener('click', event => {
+			if ( event.target.classList.contains( 'playbutton' ) ) {
+				let isPlaying = event.target.classList.contains( 'playing' );
+			    document.querySelectorAll( '.playbutton' ).forEach( el => {
+					el.classList.remove( 'playing' );
+				});
+				audioElement.pause();
+				if ( ! isPlaying ) {
+					let audioURL = event.target.getAttribute( 'data-audiourl' );
+					if ( audioElement.getAttribute( 'src' ) !== audioURL ) {
+						audioElement.setAttribute( 'src', audioURL );
+					}
+					audioElement.play();
+					event.target.classList.add( 'playing' );
 				}
-				$(this).addClass('playing');
-				audioElement.play();
-			} else {
-				$(this).removeClass('playing');
 			}
 		});
 		/* go to next track (or first) */
-		audioElement.addEventListener('ended', function() {
-			var playButton;
-			if( $('.playing').parents('li').is(':last-child') ) {
-				audioElement.setAttribute('src', $('#podcast>li:first-child').find('a').attr('href'));
-				playButton = $('#podcast>li:first-child').find('.playbutton');
+		audioElement.addEventListener( 'ended', function() {
+			let playButton = null;
+			let nextURL = null;
+			let liPlaying = document.querySelector( '.playing' ).closest( 'li' );
+			let nextSibling = liPlaying.nextElementSibling;
+			if ( nextSibling === null ) {
+				nextURL = podcast_list.querySelector( 'li:first-child a' ).getAttribute( 'href' );
+				playButton = podcast_list.querySelector( 'li:first-child button' );
 			} else {
-				audioElement.setAttribute('src', $('.playing').parents('li').next().find('a').attr('href'));
-				playButton = $('.playing').parents('li').next().find('.playbutton');
+				nextURL = nextSibling.querySelector( 'a' ).getAttribute( 'href' );
+				playButton = nextSibling.querySelector( 'button' );
 			}
-			$('.playbutton').removeClass('playing');
-			playButton.addClass('playing');
+			document.querySelectorAll( '.playbutton' ).forEach( el => {
+				el.classList.remove( 'playing' );
+			});
+			playButton.classList.add( 'playing' );
+			audioElement.setAttribute('src', nextURL);
 			audioElement.play();
 		}, false);
 	}
-	if($('.bq-list').length){
-		var types = {'cakes':[],'puddings':[],'mains':[],'dips':[],'sides':[],'salads':[],'spices':[]};
-		$('ul.bq-list li').each(function(){
-			types[$(this).data('tags')].push($(this));
-		});
-		$('.bq-list').before('<p>Sort by <a href="#" class="sort-link sort-date sort-active">date</a>, <a href="#" class="sort-link sort-az">a-z</a> or <a href="#" class="sort-link sort-type">group by type</a></p>');
-		$(document).on('click', '.sort-date', function(e){
-			e.preventDefault();
-			if ($('.bq-list').length>1) {
-				ungroup();
+	/* list filters */
+	const postList = document.querySelector( '.post-list' );
+	if ( postList !== null ) {
+		var tags = {};
+		postList.querySelectorAll( 'li' ).forEach( el => {
+			if ( ! tags.hasOwnProperty( el.getAttribute( 'data-tags' ) ) ) {
+				tags[ el.getAttribute( 'data-tags' ) ] = [];
 			}
-			$('.bq-list').removeClass('sorted-desc').removeClass('sorted-asc');
-			sortList($('.bq-list'),true,'date');
-			$('.sort-link').removeClass('sort-active');
-			$('.sort-az').text('a-z');
-			$(this).addClass('sort-active');
+			tags[ el.getAttribute( 'data-tags' ) ].push( el );
 		});
-		$(document).on('click', '.sort-az', function(e){
-			e.preventDefault();
-			if ($('.bq-list').length>1) {
-				ungroup();
+		const sortControls = document.createElement( 'p' );
+		sortControls.innerHTML = 'Sort by <a href="#" class="sort-link sort-date sort-active">date</a>, <a href="#" class="sort-link sort-az">a-z</a> or <a href="#" class="sort-link sort-type">group by type</a>';
+		postList.parentNode.insertBefore( sortControls, postList );
+		document.addEventListener( 'click', event => {
+			if ( event.target.classList.contains( 'sort-date' ) ) {
+				event.preventDefault();
+				postList.classList.remove( 'sorted-desc', 'sorted-asc' );
+				if ( document.querySelectorAll( '.post-list' ).length > 1 ) {
+					unGroup();
+				}
+				sortList( postList, true, 'date' );
+				document.querySelectorAll( '.sort-link' ).forEach( el => { el.classList.remove( 'sort-active' ); } );
+				document.querySelector( '.sort-az' ).textContent = 'a-z';
+				event.target.classList.add( 'sort-active' );
+			} else if ( event.target.classList.contains( 'sort-az' ) ) {
+				event.preventDefault();
+				let sortDescending = postList.classList.contains( 'sorted-asc' );
+				let newClass = sortDescending? 'sorted-desc': 'sorted-asc';
+				let newText =  sortDescending? 'a-z': 'z-a';
+				postList.classList.remove( 'sorted-desc', 'sorted-asc' );
+				if ( document.querySelectorAll( '.post-list' ).length > 1 ) {
+					unGroup();
+				}
+				postList.classList.add( newClass );
+				sortList( postList, sortDescending );
+				document.querySelector( '.sort-link' ).classList.remove( 'sort-active' );
+				event.target.classList.add( 'sort-active' );
+				event.target.textContent = newText;
+			} else if ( event.target.classList.contains( 'sort-type' ) ) {
+				event.preventDefault();
+				if ( document.querySelectorAll( '.post-list' ).length > 1 ) {
+					unGroup();
+				} else {
+
+				}
+
 			}
-			var sortDescending = $(this).parent().next('.bq-list').hasClass('sorted-asc');
-			var newClass = sortDescending? 'sorted-desc': 'sorted-asc';
-			var newText =  sortDescending? 'a-z': 'z-a';
-			$('.bq-list').removeClass('sorted-desc').removeClass('sorted-asc').addClass(newClass);
-			sortList($('.bq-list'),sortDescending);
-			$('.sort-link').removeClass('sort-active');
-			$(this).addClass('sort-active').text(newText);
-		});
+		});/*
 		$(document).on('click', '.sort-type', function(e){
 			e.preventDefault();
 			if ($('.bq-list').length>1) {
 				ungroup();
 			} else {
-				$('.bq-list').remove();
-				for(type in types){
-					$('main').append('<h3 class="bq-list-header">'+type.substr(0,1).toUpperCase()+type.substr(1)+'</h3>');
+				document.querySelector( '.bq-list' ).remove();
+				for ( tagli in tags ) {
+					document.getElementById( 'maincontainer' ).append('<h3 class="bq-list-header">'+type.substr(0,1).toUpperCase()+type.substr(1)+'</h3>');
 					$('main').append($('<ul class="bq-list"\>').append(types[type]));
 				}
+				document.getElementById( 'maincontainer' )
 			}
 			$('.bq-list').each(function(){
 				sortList($(this));
@@ -94,16 +136,21 @@
 			$('.sort-link').removeClass('sort-active');
 			$('.sort-az').text('a-z');
 			$(this).addClass('sort-active');
-		});
+		});*/
 	}
-	function ungroup(){
-		var newList = $('<ul class="bq-list"/>');
-		newList.append($('.bq-list').children('li').get());
-		$('.bq-list,.bq-list-header').remove();
-		$('main').append(newList);
+	function unGroup(){
+		var newList = document.createElement( 'ul' );
+		newList.classList.add( 'post-list' );
+		document.querySelectorAll( '.post-list li' ).forEach( el => {
+			newList.appendChild( el );
+		});
+		document.querySelectorAll( '.post-list,.post-list-header' ).forEach( el => {
+			el.remove();
+		});
+		postList.appendChild( newList );
 	}
 	function sortList(list, sortDescending, by) {
-		var sortdir = sortDescending? -1: 1;
+	/*	var sortdir = sortDescending? -1: 1;
 		var sortby = (by && by === 'date')? 'date': 'title';
 		var listitems = list.children('li').get();
 		listitems.sort(function(a, b) {
@@ -114,6 +161,5 @@
 			}
 		});
 		$.each(listitems, function(idx, itm) { list.append(itm); });
-	}
-	
-})(jQuery);
+	*/}
+});
